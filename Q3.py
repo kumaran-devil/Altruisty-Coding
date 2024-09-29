532ebab01547c25081371ea293a813008e6d77e1
Kumaran <91403552+kumaran-devil@users.noreply.github.com>
2024-09-29 18:39:47 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,1 +1,16 @@
+k = int(input())
+n = int(input())
+arr = []
 
+def max_of_mins(arr, k):
+    max_min = float('-inf')
+    for i in range(len(arr) - k + 1):
+        subarray = arr[i:i+k]
+        current_min = min(subarray)
+        max_min = max(max_min, current_min)
+    return max_min
+
+for _ in range(n):
+    arr.append(int(input()))
+
+print(f"[{max_of_mins(arr, k)}]")
